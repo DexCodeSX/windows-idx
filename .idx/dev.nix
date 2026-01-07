dcf3f0ac99b891dbc507a3fd2a7b555583ba8422
pdb7tsghyb-beep <pdb7tsghyb@privaterelay.appleid.com>
2026-01-06 21:47:30 -0800

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,15 +1,30 @@
 { pkgs, ... }:
 
 {
+  # Danh sách package cài sẵn
   packages = with pkgs; [
+    # QEMU đầy đủ (có qemu-system-x86_64)
     qemu_full
+
+    # Network / download
     curl
     wget
+
+    # Tunnel
+    ngrok
+
+    # Dev tools
     git
-    sudo
-    apt
+    unzip
+    busybox
+
+    # Utils (xem network, pci…)
+    iproute2
+    pciutils
+    usbutils
   ];
 
+  # Biến môi trường (an toàn với IDX)
   env = {
     QEMU_AUDIO_DRV = "none";
   };
